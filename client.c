113ff658f36946a83194285aabfa4d32a08a5326
cynthiabaran <barancynthia@gmail.com>
2018-10-17 01:29:46 -0300

    add screen to client

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include "oo_model.hpp"
 
 int main() {
   int socket_fd;
@@ -16,7 +16,6 @@ int main() {
   
   printf("IP do servidor:\n");
   scanf("%s", serverIp);	
-
   socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   printf("Socket criado\n");
 
@@ -29,23 +28,92 @@ int main() {
     return 0;
   }
   printf("Conectei ao servidor\n");
-  system("/bin/stty raw");
-  character[1] = '\0';
-  /* Agora, meu socket funciona como um descritor de arquivo usual */
-  while(character[0] != 'q'){
-    scanf(" %c",&character[0]);
-    if(character[0] != ' '){
-      send(socket_fd, character, 2, 0);
-      //printf("Enviei mensagem com o caractere %c\n", character[0]);
-    }
-  }
 
-  /* Recebendo resposta */
-  char reply[10];
-  recv(socket_fd, reply, 10, 0);
-  printf("Resposta:\n%s\n", reply);
+  int winX = 15;
+  int winY = 51;
+
+  showStartFrog();
+
+  char c,c2;//char clicado no teclado
+  char cPrev = 0;//char anterior clicado pelo teclado
+  char key = '0';
+
+  RelevantData *rd = new RelevantData(*player, *l);
+  Teclado *teclado = new Teclado();
+  teclado->init();
+
+  char *bufferchar = new char[buffer.length()+1];
+  recv(socket_fd, bufferchar, strlen(bufferchar), 0);
+  rd->unserialize(bufferchar);
+
+  // TODO: fix level
+  Tela *tela = new Tela(rd->player, rd->l, &level ,winX, winY, winX, winY);
+  tela->init();
+
+  while (1) {
+
+    recv(socket_fd, bufferchar, strlen(bufferchar), 0);
+    rd->unserialize(bufferchar);
+    
+    // Atualiza tela
+    tela->update();
+
+    // Lê o teclado
+    c = teclado->getchar();
+    if (c != cPrev){ //evita que o usuário deixe o botão pressionado para andar mais rápido
+      if (c=='w') {
+        if(player->getX() > 3)
+          player->update(player->getX()-1,player->getY());
+      }
+      if (c=='a') {
+        if(player->getY() > 1)
+          player->update(player->getX(),player->getY()-1);
+      }
+      if (c=='s') {
+        if(player->getX() < laneStartX+2)
+          player->update(player->getX()+1,player->getY());
+      }
+      if (c=='d') {
+        if(player->getY() < laneY+2)
+          player->update(player->getX(),player->getY()+1);
+      }
+      if (c=='q') {
+        break;
+      }
+      send(socket_fd, c, 2, 0);
+    }
+    cPrev = c;
+
+    c2 = key;
+    if (c2=='w') {
+      if(player->getX() > 3)
+        player->update(player->getX()-1,player->getY());
+    }
+    if (c2=='a') {
+      if(player->getY() > 1)
+        player->update(player->getX(),player->getY()-1);
+    }
+    if (c2=='s') {
+      if(player->getX() < laneStartX+2)
+        player->update(player->getX()+1,player->getY());
+    }
+    if (c2=='d') {
+      if(player->getY() < laneY+2)
+        player->update(player->getX(),player->getY()+1);
+    }
+    if (c2=='q') {
+      break;
+    }
+    send(socket_fd, c2, 2, 0);
+    key = '0';
+
+    std::this_thread::sleep_for (std::chrono::milliseconds(100));
+
+  }
+  
+  tela->stop();
+  teclado->stop();
   close(socket_fd);
-  system ("/bin/stty cooked");
   return 0;
 }
 
